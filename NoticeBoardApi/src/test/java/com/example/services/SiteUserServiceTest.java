6e36d1ea5ea1a434e1980ede5a7e576bc6858e2d
gunwoong <gwj0421@gmail.com>
2024-02-19 16:56:23 +0900

    test: User 포맷 및 CRUD 기능 테스트 추가

---
+++ NoticeBoardApi/src/test/java/com/example/services/SiteUserServiceTest.java
@@ -0,0 +1,67 @@
+package com.example.services;
+
+import com.example.config.ServiceConfig;
+import com.example.dao.SiteUser;
+import com.example.repository.SiteUserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@DataMongoTest
+@Import(ServiceConfig.class)
+@Slf4j
+class SiteUserServiceTest {
+    @Autowired
+    private SiteUserRepository userRepository;
+    @Autowired
+    private SiteUserService userService;
+
+    @BeforeEach
+    @AfterEach
+    void setUpAndTearDown() {
+        userRepository.deleteAll().block();
+    }
+
+    @Test
+    void Should_returnSiteUser_When_givenUserInfo() {
+        // given
+        SiteUser newUser = new SiteUser("testName", "testUserId", "testPassword", "testEmail@gamil.com");
+
+        // when
+        Mono<SiteUser> createdUser = userService.createSiteUser(newUser);
+        Mono<SiteUser> getUser = userService.getUserByUserId("testUserId");
+
+        // then
+        StepVerifier.create(createdUser)
+                .expectNextMatches(retrievedUser -> retrievedUser.getId() != null)
+                .verifyComplete();
+
+        StepVerifier.create(getUser)
+                .expectNextMatches(retrievedUser -> retrievedUser.getId() != null && retrievedUser.getUserId().equals("testUserId"))
+                .verifyComplete();
+    }
+
+
+    @Test
+    void Should_deleteUser_When_givenUserInfo() {
+        // given
+        SiteUser newUser = new SiteUser("testName", "testUserId", "testPassword", "testEmail@gamil.com");
+
+        // when
+        Mono<Void> deleteUser = userService.createSiteUser(newUser).flatMap(user -> userService.deleteUser(user.getId()));
+        Mono<SiteUser> getUser = userService.getUserByUserId("testUserId");
+
+        // then
+        StepVerifier.create(deleteUser)
+                .verifyComplete();
+        StepVerifier.create(getUser)
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+}
