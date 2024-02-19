0a533d246a47e41f9ca2f26e87a8142260672f2e
gunwoong <gwj0421@gmail.com>
2024-02-19 17:04:09 +0900

    feat: spring 기본 구조 구현

---
+++ NoticeBoardApi/src/main/java/com/example/config/MongoReactiveApplication.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+@EnableReactiveMongoRepositories(basePackages = "com.example.repository")
+@EnableReactiveMongoAuditing
+public class MongoReactiveApplication extends AbstractReactiveMongoConfiguration {
+    @Value("spring.data.mongodb.uri")
+    private String mongoUri;
+
+    @Override
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "wise-words-scanner-db";
+    }
+}
