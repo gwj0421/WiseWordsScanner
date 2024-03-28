bb5adb53e2b4e7691fe524fc3b6ecdfb39311a3c
gunwoong <gwj0421@gmail.com>
2024-03-28 18:56:13 +0900

    feat: 댓글과 답글 기능 추가

--- NoticeBoardApi/src/main/java/com/example/services/PageService.java
+++
@@ -1,8 +0,0 @@
-package com.example.services;
-
-import com.example.dto.PostPageForm;
-import reactor.core.publisher.Mono;
-
-public interface PageService {
-    Mono<PostPageForm> getPostPageByPostId(String postId);
-}
--- NoticeBoardApi/src/main/java/com/example/services/PostService.java
+++ NoticeBoardApi/src/main/java/com/example/services/PostService.java
@@ -3,12 +3,12 @@ package com.example.services;
 import com.example.dao.Post;
 import com.example.dto.PageablePostsResponse;
 import com.example.dto.PostForm;
+import com.example.dto.PostPageForm;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface PostService {
-    Mono<Post> createPost(Post post);
     Mono<PostForm> createPost(PostForm postForm, ServerHttpRequest request);
     Mono<Post> readPostById(String id);
     Flux<Post> readPostsByAuthorId(String userId);
@@ -16,8 +16,8 @@ public interface PostService {
     Mono<Void> recommendPost(String postId, String recommenderId,boolean userRecommend);
     Mono<PageablePostsResponse> getPostsByPage(int pageNumber, int pageSize);
     Mono<PageablePostsResponse> getPostsBySearch(String keyword,int page, int size);
-
-
     Mono<PostForm> getPostDetailById(String id);
+    Mono<PostPageForm> getPostPageByPostId(String postId);
     Mono<Void> deletePostById(String id);
+    
 }
